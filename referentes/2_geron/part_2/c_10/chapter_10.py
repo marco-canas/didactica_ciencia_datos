2e217eab109970c625f88c9e3df7afe138ad6e9c
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2022-10-31 16:45:17 -0500

    actualización de figuras

--- referentes/2_geron/part_2/c_10/chapter_10.py
+++ referentes/2_geron/part_2/c_10/chapter_10.py
@@ -8,5 +8,19 @@ def obtener_datos_fashion_mnist():
     return (X_train_full, y_train_full), (X_test, y_test)
 
 
+def graficar_datos():
+    from sklearn.datasets import load_iris 
+    X, y = load_iris(return_X_y = True) 
+    X = X[:,(2,3)] 
+    import matplotlib.pyplot as plt 
+    ax = plt.gca() 
+    ax.set(title = 'Visualización de datos de flores de iris', xlabel = 'Longitud del pétalo')
+    ax.set_ylabel('ancho del pétalo')
+    ax.axis( [ min(X[:, 0])-0.3, max(X[:,0])+0.3, min(X[:, 1])-0.3, max(X[:,1])+ 0.3 ]) 
+    ax.plot(X[:, 0], X[:,1], 'o')
+    ax.grid() 
+    plt.savefig('Todo_el_dataset-iris.jpg')
+     
+
 if __name__=='__main__':
     pass 
