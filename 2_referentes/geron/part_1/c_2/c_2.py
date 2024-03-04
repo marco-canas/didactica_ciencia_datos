b138a06d2f12b84964f82f4ecf65fe1606e6de56
Marco Cañas <marco.canas@udea.edu.co>
2024-03-04 12:39:53 -0500

    simplificada del capítulo 2

--- 2_referentes/geron/part_1/c_2/c_2.py
+++
@@ -1,8 +0,0 @@
-def dividir_entrenamiento_testeo(datos, tasa_testeo):
-    from sklearn.model_selection import train_test_split
-    datos_train, datos_test = train_test_split(datos, test_size=tasa_testeo ) 
-    return datos_train, datos_test
-
-
-if __name__=='__main__':
-    pass 
